14b8bd15cf977dc6fde974c8781082e39fc914e3
Michael Moriarty <mmoriarty316@gmail.com>
2021-04-02 17:26:00 -0700

    initialized skeleton folder structure, express server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = require('./routes.js');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use('/', router);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ server/routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+// flight cheapest date search (to, from) => cheapest dates to fly from to
+// flight offers search (date to, date from, to, from) => cheapest flights
+// flight offers price: checks final price for flight (constanly updates)
+// flight inspo: cheapest flights from X (location) => cheapest places to fy from X
+module.exports = router;
